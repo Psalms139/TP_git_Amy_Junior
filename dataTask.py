716a194ee9362c91319f358f5065a6770c7a2b80
kkk083 <junbi083@gmail.com>
2024-03-07 12:41:24 +0400

    2eme fonction creee sur bdd

--- dataTask.py
+++ dataTask.py
@@ -1,8 +1,13 @@
 import json
 
 def enregistrer_json (dictionnaire, chemin_fichier):
-	with open(chemin_fichier, 'w')
+	with open(chemin_fichier, 'w') as fichier:
+		json.dump(dictionnaire, fichier)
 
+def charger_json (chemin_fichier):
+	with open(chemin_fichier, 'r') as fichier:
+		contenu_json = json.load(fichier)
+	return contenu_json
 
 if __name__== "__main__":
 	dict={"toto":42}
