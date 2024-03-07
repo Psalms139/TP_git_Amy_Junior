8f95d0d5054921aefdd3c64cc4b0a21eca7a7eff
kkk083 <junbi083@gmail.com>
2024-03-07 12:33:32 +0400

    fonction creee sur bdd

---
+++ dataTask.py
@@ -0,0 +1,9 @@
+import json
+
+def enregistrer_json (dictionnaire, chemin_fichier):
+	with open(chemin_fichier, 'w')
+
+
+if __name__== "__main__":
+	dict={"toto":42}
+	enregistrer_json(dict,"/home/student/")
