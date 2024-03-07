044e966f5b7f67e3d0238ddb7709768343e67d51
kkk083 <junbi083@gmail.com>
2024-03-07 14:26:33 +0400

    nouveau programme

---
+++ taskDisplay.py
@@ -0,0 +1,6 @@
+def AjouterValeur (dico, cle, valeur):
+	dico[cle] = valeur
+
+print(" 1. Charger une liste de taches\n2. Creer une liste de tache")
+
+choice = input("Que voulez vous faire ? 1 ou 2")
