7cd3f123475082897b789d87a96ce5ab79048396
amy <amynomenaratsihoarana@gmail.com>
2024-03-07 13:12:38 +0400

    3 eme fonction TacheFinie

--- TaskTool.py
+++ TaskTool.py
@@ -3,11 +3,19 @@ def AjouterValeur  (dico, cle, valeur) :
 
 def Suppression (dico, cle ):
         del dico [cle]
+def TacheFinie(tache):
+	if tache in MonDico :
+	MonDico [tache]+= ("Terminee")
+	else :
+	print("La tache n'existe pas")
+
 
 
 MonDico = {'name' : "John", 'age' : "31"} 
 
 AjouterValeur(MonDico, 'ville', 'RH')
 Suppression (MonDico, 'name')
+TacheFinie ('name') 
+
 print (MonDico)
 
