9c712b6964dc2a571fc432790f37d68ee53257ae
kkk083 <junbi083@gmail.com>
2024-03-07 14:50:36 +0400

    new update

--- taskDisplay.py
+++ taskDisplay.py
@@ -2,5 +2,14 @@ def AjouterValeur (dico, cle, valeur):
 	dico[cle] = valeur
 
 print(" 1. Charger une liste de taches\n2. Creer une liste de tache")
-
 choice = input("Que voulez vous faire ? 1 ou 2")
+
+if choix.lower() == 1:
+	charger_list_tache()
+elif choix.lower() == 2:
+	creer_liste_taches():
+else:
+	print("choix invalide.")
+
+if __name__ == "__main__":
+	main()
